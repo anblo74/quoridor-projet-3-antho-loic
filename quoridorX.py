535b2a85e6846cdd84c7bdea6cf27b63f74c1b67
Anthony Blouin <anblo74@ulaval.ca>
2020-12-05 17:13:28 -0500

    Ajout du plan pour l'affichage graphique

--- quoridorX.py
+++ quoridorX.py
@@ -16,5 +16,22 @@ Elle remplace alors le print() en art ASCII de la classe mère Quoridor
 Elle affiche à l'aide de la librairie turtle une représentation graphique du damier dans une fenêtre externe
 Explication de l'implantation:
     Importer la librairie turtle
-    TODO
+    Faire le setup avec une résolution de 800 x 800
+    Définir une relation entre la position du damier et les pixels
+    Générer le board de base
+        Faire un carré
+        Délimiter les cases
+        Ajouter les numéros de case
+    Ajouter des éléments selon l'état de la partie
+        TODO
+
 """
+
+class QuoridorX(Quoridor):
+
+    def __str__(self):
+        damier = turtle.Screen()
+        damier.title("Jeu de Quoridor")
+        damier.setup(width=800, height=800)
+        
+        tony = turtle.Turtle()
